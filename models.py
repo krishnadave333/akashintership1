5f99a4e80318334fcd0e3d92abe7c2800a4842e1
krishnadave333 <84893135+krishnadave333@users.noreply.github.com>
2021-06-12 05:04:36 -0700

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-
-class Todouser(models.Model):
-    user_todo = models.OneToOneField(User, default=None, null=True, on_delete=models.CASCADE)
-    title=models.CharField(max_length=25,null=False)
-    description=models.CharField(max_length=100,null=False)
